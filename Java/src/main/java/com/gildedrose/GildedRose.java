b1af3e795df1c5053bc5ecde6942ead0ce5fd2db
김동진 <dongjin403@naver.com>
2025-02-20 21:04:46 +0900

    refactor : 중복 내용 변수 처리

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -3,9 +3,9 @@ package com.gildedrose;
 class GildedRose {
     Item[] items;
 
-    private static final String AGED_BRIE = "Aged Brie";
-    private static final String BACK_STAGE = "Backstage passes to a TAFKAL80ETC concert";
-    private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+    private static final String AGED_BRIE = "Aged Brie"; //시간 지나면 Quility 올라감
+    private static final String BACK_STAGE = "Backstage passes to a TAFKAL80ETC concert"; // 시간 지나면 값 올라가다가 0
+    private static final String SULFURAS = "Sulfuras, Hand of Ragnaros"; // 값이 아예 안떨어짐
 
     public GildedRose(Item[] items) {
         this.items = items;
@@ -13,51 +13,56 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (!item.name.equals(AGED_BRIE)
-                && !item.name.equals(BACK_STAGE)) {
-                if (item.quality > 0) {
-                    if (!item.name.equals(SULFURAS)) {
-                        item.quality -= 1;
+            boolean agedBrie = item.name.equals(AGED_BRIE);
+            boolean backStage = item.name.equals(BACK_STAGE);
+            boolean sulfuras = item.name.equals(SULFURAS);
+            int quality = item.quality;
+            int sellIn = item.sellIn;
+
+            if (!agedBrie && !backStage) { //일반적인 경우일때
+                if (quality > 0) {
+                    if (!sulfuras) {
+                        quality -= 1;
                     }
                 }
-            } else {
-                if (item.quality < 50) {
-                    item.quality += 1;
+            } else { // quality가 상승해야될때
+                if (quality < 50) {
+                    quality += 1;
 
-                    if (item.name.equals(BACK_STAGE)) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality += 1;
+                    if (backStage) {
+                        if (sellIn < 11) {
+                            if (quality < 50) {
+                                quality += 1;
                             }
                         }
 
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality += 1;
+                        if (sellIn < 6) {
+                            if (quality < 50) {
+                                quality += 1;
                             }
                         }
                     }
                 }
             }
 
-            if (!item.name.equals(SULFURAS)) {
-                item.sellIn -= 1;
+            if (!sulfuras) {
+                sellIn -= 1;
             }
 
-            if (item.sellIn < 0) {
-                if (!item.name.equals(AGED_BRIE)) {
-                    if (!item.name.equals(BACK_STAGE)) {
-                        if (item.quality > 0) {
-                            if (!item.name.equals(SULFURAS)) {
-                                item.quality -= 1;
+            if (sellIn < 0) {
+                if (!agedBrie) {
+                    if (!backStage) {
+                        if (quality > 0) {
+                            if (!sulfuras) {
+                                quality -= 1;
                             }
                         }
                     } else {
-                        item.quality = 0;
+                        quality = 0;
                     }
                 } else {
-                    if (item.quality < 50) {
-                        item.quality += 1;
+                    if (quality < 50) {
+                        quality += 1;
                     }
                 }
             }
